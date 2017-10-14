0ac30c8ab612b6af928bcb29e76bd4d84fa48b32
Jumar Martin <jumar@jumart.in>
2017-10-14 19:00:04 -0400

    abstracted the finding extensions step into it's own methodgit add .! cleaned up a couple things.

--- cleanup.py
+++ cleanup.py
@@ -1,37 +1,42 @@
-
+"""Imports os for os operations, shutil to move files"""
 import os
 import shutil
-import re as regex
 
-desktopDirectory = os.path.join(os.environ.get('HOME'), 'Desktop')
-
-os.chdir(desktopDirectory)
-
-desktopContents = os.listdir()
-
-extArray = []
-
-# Finding extension(s)
-for file in desktopContents:
-    ext = os.path.splitext(file)[1]
-    if os.path.isfile(file) == True:
-        if not file.startswith('.'):
-            if ext not in extArray:
-                extArray.append(ext)
+DESKTOPDIRECTORY = os.path.join(os.environ.get('HOME'), 'Desktop')
 
 
-# Make folder for extension(s)
-for extension in extArray:
-    extension = extension.replace('.', '')
-    os.makedirs(os.path.join(desktopDirectory, extension), exist_ok=True)
-    for file in desktopContents:
-        if os.path.isfile(file) == True:
-            if extension in file:
-                if os.path.exists(desktopDirectory):
-                    shutil.move(os.path.join(desktopDirectory, file),
-                                os.path.join(desktopDirectory, extension))
+def cleanup():
+    """Cleans up the desktop NEED TO ABSTRACT MORE"""
+    os.chdir(DESKTOPDIRECTORY)
+    desktopcontents = os.listdir()
+    extarray = []
+
+    findextensions(desktopcontents, extarray)
+
+    # Make folder for extension(s)
+    for extension in extarray:
+        extension = extension.replace('.', '')
+        os.makedirs(os.path.join(DESKTOPDIRECTORY, extension), exist_ok=True)
+        for file in desktopcontents:
+            if os.path.isfile(file):
+                if extension in file:
+                    if os.path.exists(DESKTOPDIRECTORY):
+                        shutil.move(
+                            os.path.join(DESKTOPDIRECTORY, file),
+                            os.path.join(DESKTOPDIRECTORY, extension))
 
 
+def findextensions(desktopcontents, extarray):
+    """Finding extension(s) on home directory"""
+    for file in desktopcontents:
+        ext = os.path.splitext(file)[1]
+        if os.path.isfile(file):
+            if not file.startswith('.'):
+                if ext not in extarray:
+                    extarray.append(ext)
+
+
+cleanup()
 # ls = os.popen('ls ~/Desktop').read()
 # regexFileExtensions = regex.compile(r"(\.\w{1,}$[^\d#~])", regex.MULTILINE)
 # items = regexFileExtensions.findall(ls)
